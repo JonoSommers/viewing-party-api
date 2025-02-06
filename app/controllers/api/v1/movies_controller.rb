6f437e4bbb120c1be80e74ecefc5335ef6d46e1d
Jono Sommers <jonosommers95@gmail.com>
2025-02-05 19:02:16 -0600

    trying to get heroku to display the correct data when query param title is input

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::MoviesController < ApplicationController
     def index
-        movies = params[:title] ? MovieGateway.movies_title_search(params[:title]) : MovieGateway.top_rated_movies
+        if params[:title]
+            movies = MovieGateway.movies_title_search(params[:title])
+        else
+            movies = MovieGateway.top_rated_movies
+        end
+    
         render json: MovieSerializer.format_movie(movies)
     end
+    
 end
