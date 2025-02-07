a5d2c3a7f3db595027274587e61d013f122f7bbc
Jono Sommers <jonosommers95@gmail.com>
2025-02-06 19:55:22 -0600

    wrote gateways tests for movies

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,7 +74,8 @@ Shoulda::Matchers.configure do |config|
   VCR.configure do |config|
     config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
     config.hook_into :webmock
-    config.filter_sensitive_data('<PEXELS_API_KEY>') { Rails.application.credentials.themoviedb[:key] }
+    config.filter_sensitive_data('<TMDB_API_KEY>') { Rails.application.credentials.themoviedb[:key] }
+    config.default_cassette_options = { re_record_interval: 1.day }
     config.configure_rspec_metadata!
   end
 end
