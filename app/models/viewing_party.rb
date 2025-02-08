29eb52e020a94053aa8b66b9b402b452d8a8e7b1
Jono Sommers <jonosommers95@gmail.com>
2025-02-08 15:14:00 -0600

    built out error handling for sad paths

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -10,7 +10,13 @@ class ViewingParty < ApplicationRecord
 
     def self.add_invitees_to_joins_table(invitees_param, party)
         invitees_param.each do |invitee_id|
-            UsersViewingParty.create!(user_id: invitee_id, viewing_party_id: party.id, host: false)
+            UsersViewingParty.create(user_id: invitee_id, viewing_party_id: party.id, host: false)
+        end
+    end
+
+    def check_if_party_ends_before_starting(starting_time, ending_time)
+        if starting_time > ending_time
+            raise StandardError, 'Your start time cannot be after your end time'
         end
     end
 end
--- app/serializers/viewing_party_serializer.rb
+++ app/serializers/viewing_party_serializer.rb
@@ -3,8 +3,12 @@ class ViewingPartySerializer
     attributes :name, :start_time, :end_time, :movie_id, :movie_title
 
     attribute :invitees do |viewing_party|
-        invitees = User.joins(:users_viewing_parties).where(users_viewing_parties: { host: false} )
-        invitees.map do |user|
+        invitees = viewing_party.users_viewing_parties.where(host:false)
+        users = invitees.map do |invitee|
+            invitee.user
+        end
+        valid_users = users.compact
+        valid_users.map do |user|
             {
                 id: user.id,
                 name: user.name,
