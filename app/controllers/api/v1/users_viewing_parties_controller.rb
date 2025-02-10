45692afd6506415284b7a02135696256380f0dda
Jono Sommers <jonosommers95@gmail.com>
2025-02-10 17:11:33 -0600

    partially completed EP4, did not have time to account for sad paths

--- app/controllers/api/v1/users_viewing_parties_controller.rb
+++ app/controllers/api/v1/users_viewing_parties_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::UsersViewingPartiesController < ApplicationController
     def update
-        user = User.find(params[:id])
-        viewing_party = ViewingParty.find(params[:id])
-        UsersViewingParty.create(user_id: user.id, viewing_party_id: viewing_party.id, host: false)
-        render json: ViewingPartySerializer.new(viewing_party)
+        UsersViewingParty.add_new_invitee_to_party(params[:user_id], params[:viewing_party_id], params[:id], params[:invitee_id])
+        render json: ViewingPartySerializer.new(ViewingParty.find(params[:viewing_party_id]))
     end
 end
--- spec/models/users_viewing_party_spec.rb
+++ spec/models/users_viewing_party_spec.rb
@@ -30,4 +30,28 @@ RSpec.describe "Users viewing Party Model Methods", type: :request do
             expect(users_viewing_party.host).to be(true)
         end
     end
+
+    describe '#self.add_new_invitee_to_party(user_id, party, uvp_id, invitee_id)' do
+        it 'should add a new invitee to an existing viewing party' do
+            user = User.create(name: "Danny DeVito", username: "danny_de_v", password: "qwert12345")
+            viewing_party = ViewingParty.create!(
+                name: "Jono's Bday Movie Bash!", 
+                start_time: "2025-02-01 14:30:00", 
+                end_time: "2025-02-01 16:30:00", 
+                movie_id: 278, 
+                movie_title: "The Shawshank Redemption"
+                )
+                
+            users_viewing_party = UsersViewingParty.new(user_id: user.id, viewing_party_id: viewing_party.id, host: nil)
+            users_viewing_party.set_host_to_true
+
+            invitee = User.create!(name: "Vanny VeDito", username: "Vanny_ve_d", password: "12345qwert")
+
+            updated_uvp = UsersViewingParty.add_new_invitee_to_party(user.id, viewing_party.id, users_viewing_party.id, invitee.id)
+
+            expect(updated_uvp.user_id).to eq(invitee.id)
+            expect(updated_uvp.viewing_party_id).to eq(viewing_party.id)
+            expect(updated_uvp.host).to be(false)
+        end
+    end
 end
